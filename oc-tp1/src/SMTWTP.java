29b5e8f20990896836e55ba9a2e33825e589bedd
Vasilev Martin <vasilev@b04p15.fil.univ-lille1.fr>
2019-09-06 13:58:39 +0200

    Score calculation

--- oc-tp1/src/SMTWTP.java
+++ oc-tp1/src/SMTWTP.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SMTWTP {
@@ -53,20 +54,29 @@ public class SMTWTP {
         return l;
     }
 
-    public static List<List<Integer>> sortInstancePerDueDate(List<List<Integer>> instance){
-        List<List<Integer>> res = new LinkedList<>();
-        List<Integer> dueDates = instance.get(2);
+    public static Integer score(List<Integer> solution, List<List<Integer>> instance) {
+        int time = 0, res = 0;
+        for (Integer job : solution) {
+            time += instance.get(0).get(job);
+            res += Integer.max(time - instance.get(2).get(job), 0) * instance.get(1).get(job);
+        }
         return res;
+    }
+
+    public static List<Integer> sortInstancePerDueDate(List<List<Integer>> instance) {
+        Collections.sort(instance, new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.get(3) > o2.get(3) ? -1 : (o1.get(3) < o2.get(3)) ? 1 : 0;
+            }
+        });
+        return instance.stream().map(job -> job.get(0)).collect(Collectors.toCollection(LinkedList::new));
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         List<List<List<Integer>>> res = SMTWTP.parseFile("wt100.txt", 100);
-        System.out.println(res.size());
-        System.out.println(res.get(1).size());
-        System.out.println(res.get(1).get(1).size());
-
-        System.out.println(SMTWTP.randomSolution(100).size());
-        System.out.println(SMTWTP.formatInstance(res.get(1), 100));
+        List<List<Integer>> formattedInstance = SMTWTP.formatInstance(res.get(1), 100);
+        System.out.println(SMTWTP.score(SMTWTP.sortInstancePerDueDate(formattedInstance),res.get(1)));
     }
 
 }
