e73c3cc28bd420edae3c459e92baa1308706226a
Vasilev Martin <vasilev@b04p15.fil.univ-lille1.fr>
2019-09-09 11:21:16 +0200

    Insertion neighbors

--- oc-tp1/src/Neighborhoods.java
+++ oc-tp1/src/Neighborhoods.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Neighborhoods {
 
@@ -50,11 +52,19 @@ public class Neighborhoods {
 
     public static List<List<Integer>> insertion(List<Integer> solution){
         List<List<Integer>> neighbors = new ArrayList<>();
-        for (int i=0; i<solution.size(); i++){
+        for (int i=0; i<solution.size()-1; i++){
             for (int j=i+1; j<solution.size(); j++){
                 if(i!=j){
                     List<Integer> copy_sol = new ArrayList<>(solution);
-                    //List<Integer> n = copy_sol.subList(0, i).add(copy_sol.get(j));
+                    List<Integer> n = Stream.concat(
+                            copy_sol.subList(0, i).stream(),
+                            Stream.concat(
+                                    Stream.of(copy_sol.get(j)),
+                                    Stream.concat(
+                                            copy_sol.subList(i,j).stream(),
+                                            copy_sol.subList(j+1,copy_sol.size()).stream())))
+                            .collect(Collectors.toList());
+                    neighbors.add(n);
                 }
             }
         }
